9e443d71b0d6fe40924064c58a6c8b99eda8ca01
Clark Zhang <mail@clarkok.com>
2016-04-20 16:55:18 +0800

    update main function to parse arguments

--- src/cript.c
+++ src/cript.c
@@ -13,18 +13,86 @@
 
 #include "inst_output.h"
 
+static void
+output_help()
+{
+    printf(
+        "Cript %s\n"
+        "Usage:"
+        "  cript [options] source.cr\n"
+        "\n"
+        "Options:"
+        "  -c\tprint compiled ir before execute\n"
+        "  -h\tprint this help message and exit\n"
+        "  -v\tprint current version and exit\n",
+        CRIPT_VERSION
+    );
+}
+
 int
 main(int argc, const char **argv)
 {
-    if (argc != 2) {
+    const char *source = NULL;
+    int output_insts = 0;
+
+    --argc; ++argv;
+    while (argc) {
+        const char *arg = *argv;
+        if (arg[0] == '-') {
+            switch (arg[1]) {
+                case 'v':
+                {
+                    printf(
+                        "Cript version %s\n"
+                        "Clarkok Zhang(mail@clarkok.com)\n",
+                        CRIPT_VERSION
+                    );
+                    return 0;
+                }
+                case 'c':
+                {
+                    output_insts = 1;
+                    break;
+                }
+                case 'h':
+                    output_help();
+                    return 0;
+                default:
+                {
+                    printf(
+                        "Unknown argument: %s\n",
+                        arg
+                    );
+                    output_help();
+                    return -1;
+                }
+            }
+        }
+        else {
+            if (source) {
+                error("Multiple source file\n  Use import instead\n");
+                return -1;
+            }
+            else {
+                source = arg;
+            }
+        }
+        --argc; ++argv;
+    }
+
+    if (!source) {
         error("No source file\n");
         return -1;
     }
-
-    ParseState *parse_state = parse_state_new_from_file(argv[1]);
+    ParseState *parse_state = parse_state_new_from_file(source);
     parse(parse_state);
 
     VMState *vm = cvm_state_new_from_parse_state(parse_state);
+
+    if (output_insts) {
+        output_vm_state(stdout, vm);
+    }
+
     lib_register(vm);
     cvm_state_run(vm);
 
